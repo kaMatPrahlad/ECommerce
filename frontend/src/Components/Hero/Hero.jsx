0d659734cd10e8292b5599d7bb1e613819a301b9
kaMatPrahlad <kamatprahlad43@gmail.com>
2025-11-30 00:49:55 +0530

    image updated

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Hero.css";
 import hand_icon from "../../Assets/hand_icon.png";
 import arrow_icon from "../../Assets/arrow_icon.png";
-import men_webp from "../../Assets/arrow_icon.png";
+import men from "../../Assets/men.webp";
 
 const Hero = () => {
   return (
@@ -21,10 +21,9 @@ const Hero = () => {
       <div className="hero-latest-btn">
         <div>Latest Collection</div>
         <img src={arrow_icon} alt="" height="30px" />
-
       </div>
       <div className="hero-right">
-        <img src={men_icon} alt="" height="30px" />
+        <img src={men} alt="" height="30px" />
       </div>
     </div>
   );
